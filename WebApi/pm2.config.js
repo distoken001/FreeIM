72f35ca932dc248ee7535e1292dc7038c28a637f
SouvernirLiu <songnian.liu@outlook.com>
2023-07-25 17:56:23 +0800

    修改

--- WebApi/pm2.config.js
+++ WebApi/pm2.config.js
@@ -8,12 +8,14 @@
             autorestart: true,
             env_development: {
                 ASPNETCORE_ENVIRONMENT: "Development",
+                PORT: 5001 // 设置生产环境下应用程序监听的端口号
             },
             // env_testing: {
             // ASPNETCORE_ENVIRONMENT: "Testing",
             //   },
             env_production: {
                 ASPNETCORE_ENVIRONMENT: "Production",
+                PORT: 5001// 设置生产环境下应用程序监听的端口号
             },
         },
     ],
