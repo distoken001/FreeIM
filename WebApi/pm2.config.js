1965e4ca3ce9783d3847d247c18f5093cf7b67bd
SouvernirLiu <songnian.liu@outlook.com>
2023-07-25 16:07:07 +0800

    修改

---
+++ WebApi/pm2.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    apps: [
+        {
+            name: "WebApi",
+            cmd: "dotnet",
+            args: "WebApi.dll",
+            watch: false,
+            autorestart: true,
+            env_development: {
+                ASPNETCORE_ENVIRONMENT: "Development",
+            },
+            // env_testing: {
+            // ASPNETCORE_ENVIRONMENT: "Testing",
+            //   },
+            env_production: {
+                ASPNETCORE_ENVIRONMENT: "Production",
+            },
+        },
+    ],
+};
